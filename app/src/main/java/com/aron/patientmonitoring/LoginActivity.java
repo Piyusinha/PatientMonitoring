5c1eadbb197e3fe7155f6ec309423ef6787495d9
piyush <piyush.sinha12345@gmail.com>
2019-11-08 02:24:56 +0530

    login and splash added

--- app/src/main/java/com/aron/patientmonitoring/LoginActivity.java
+++ app/src/main/java/com/aron/patientmonitoring/LoginActivity.java
@@ -3,12 +3,26 @@ package com.aron.patientmonitoring;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class LoginActivity extends AppCompatActivity {
+    RelativeLayout relay1;
+    Handler handler =new Handler();
+    Runnable runnable=new Runnable() {
+        @Override
+        public void run() {
+          relay1.setVisibility(View.VISIBLE);
 
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        relay1=(RelativeLayout)findViewById(R.id.rllogin);
+        handler.postDelayed(runnable,2000);
+
     }
 }
