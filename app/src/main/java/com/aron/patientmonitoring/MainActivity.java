bfc511b9c1b81d9ef2d46dad179fd27581803271
piyush <piyush.sinha12345@gmail.com>
2019-11-14 23:30:46 +0530

    Firebase ADDED

--- app/src/main/java/com/aron/patientmonitoring/MainActivity.java
+++ app/src/main/java/com/aron/patientmonitoring/MainActivity.java
@@ -1,19 +1,49 @@
 package com.aron.patientmonitoring;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 Button btn;
+TextView humidity;
+DatabaseReference mRootref= FirebaseDatabase.getInstance().getReference();
+DatabaseReference mHumidityref=mRootref.child("Humidity");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        humidity=(TextView)findViewById(R.id.humidity);
 
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mHumidityref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String text=dataSnapshot.getValue(String.class);
+                humidity.setText(text);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
