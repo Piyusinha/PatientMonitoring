57e7873e1d4341df21e9d42c00091123972faf3b
piyush <piyush.sinha12345@gmail.com>
2019-11-15 01:04:34 +0530

    Notification ADDED

--- app/src/main/java/com/aron/patientmonitoring/MainActivity.java
+++ app/src/main/java/com/aron/patientmonitoring/MainActivity.java
@@ -2,9 +2,17 @@ package com.aron.patientmonitoring;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.app.TaskStackBuilder;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -16,6 +24,10 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+    private final String CHANNEL_ID ="patient_notification";
+    private final int NOTIFICATION_ID=001;
+    private final String CHANNEL_NAME="PATIENT MONITORING";
+    private final String CHANNEL_DESC="ALERT";
 Button btn;
 TextView humidity;
 DatabaseReference mRootref= FirebaseDatabase.getInstance().getReference();
@@ -24,9 +36,44 @@ DatabaseReference mHumidityref=mRootref.child("Humidity");
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.O)
+        {
+            NotificationChannel channel=new NotificationChannel(CHANNEL_ID,CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription(CHANNEL_DESC);
+            NotificationManager manager=getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+
+
         humidity=(TextView)findViewById(R.id.humidity);
+        btn =(Button)findViewById(R.id.check);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayNotification(400);
+
+            }
+        });
 
 
+    }
+    public void displayNotification(float humidityValue)
+    {
+        Intent resultIntent = new Intent(this, MainActivity.class);
+// Create the TaskStackBuilder and add the intent, which inflates the back stack
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addNextIntentWithParentStack(resultIntent);
+// Get the PendingIntent containing the entire back stack
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder builder=new NotificationCompat.Builder(this,CHANNEL_ID);
+        builder.setSmallIcon(R.drawable.notification_patient);
+        builder.setContentTitle("Humidity Alert");
+        builder.setContentText(String.valueOf(humidityValue));
+        builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        builder.setContentIntent(resultPendingIntent);
+        NotificationManagerCompat notificationManagerCompat=NotificationManagerCompat.from(this);
+        notificationManagerCompat.notify(NOTIFICATION_ID,builder.build());
     }
 
     @Override
@@ -35,8 +82,13 @@ DatabaseReference mHumidityref=mRootref.child("Humidity");
         mHumidityref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                String text=dataSnapshot.getValue(String.class);
-                humidity.setText(text);
+                Float text=dataSnapshot.getValue(Float.class);
+                humidity.setText(String.valueOf(text));
+
+                if(text>300)
+                {
+                    displayNotification(text);
+                }
 
             }
 
