1f858a64c6d6ab54bed55b7caf36acc1db0f1e52
piyush <piyush.sinha12345@gmail.com>
2019-11-22 00:54:04 +0530

    ui updated all firebase added

--- app/src/main/java/com/aron/patientmonitoring/MainActivity.java
+++ app/src/main/java/com/aron/patientmonitoring/MainActivity.java
@@ -24,21 +24,30 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
     private final String CHANNEL_ID ="patient_notification";
     private final int NOTIFICATION_ID=001;
     private final String CHANNEL_NAME="PATIENT MONITORING";
     private final String CHANNEL_DESC="ALERT";
-LottieAnimationView  lottieAnimationView;
+LottieAnimationView  lottieAnimationViewHumidity,lottieAnimationViewheart,lottieAnimationViewpulse,lottieAnimationViewtemp;
 Button btn;
-TextView humidity;
+TextView humidity,heart,pulse,temp;
 DatabaseReference mRootref= FirebaseDatabase.getInstance().getReference();
 DatabaseReference mHumidityref=mRootref.child("Humidity");
+DatabaseReference mHeartrateref=mRootref.child("Heartrate");
+DatabaseReference mTemperature=mRootref.child("Temperature");
+DatabaseReference mPulse = mRootref.child("Pulse");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        lottieAnimationView =(LottieAnimationView)findViewById(R.id.loadinghumidity);
+        lottieAnimationViewHumidity =(LottieAnimationView)findViewById(R.id.loadinghumidity);
+        lottieAnimationViewheart=(LottieAnimationView)findViewById(R.id.loadinghr);
+        lottieAnimationViewpulse=(LottieAnimationView)findViewById(R.id.loadingpulse);
+        lottieAnimationViewtemp=(LottieAnimationView)findViewById(R.id.loadingtemperature);
+
         if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.O)
         {
             NotificationChannel channel=new NotificationChannel(CHANNEL_ID,CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
@@ -49,11 +58,14 @@ DatabaseReference mHumidityref=mRootref.child("Humidity");
 
 
         humidity=(TextView)findViewById(R.id.humidity);
+        heart=(TextView)findViewById(R.id.heartrate);
+        pulse=(TextView)findViewById(R.id.pulse);
+        temp=(TextView)findViewById(R.id.temperature);
 
 
 
     }
-    public void displayNotification(float humidityValue)
+    public void displayNotificationhumidity(float humidityValue)
     {
         Intent resultIntent = new Intent(this, MainActivity.class);
 // Create the TaskStackBuilder and add the intent, which inflates the back stack
@@ -71,6 +83,60 @@ DatabaseReference mHumidityref=mRootref.child("Humidity");
         NotificationManagerCompat notificationManagerCompat=NotificationManagerCompat.from(this);
         notificationManagerCompat.notify(NOTIFICATION_ID,builder.build());
     }
+    public void displayNotificationheart(String heartrate)
+    {
+        Intent resultIntent = new Intent(this, MainActivity.class);
+// Create the TaskStackBuilder and add the intent, which inflates the back stack
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addNextIntentWithParentStack(resultIntent);
+// Get the PendingIntent containing the entire back stack
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder builder=new NotificationCompat.Builder(this,CHANNEL_ID);
+        builder.setSmallIcon(R.drawable.notification_patient);
+        builder.setContentTitle("Heart Rate High Alert");
+        builder.setContentText(heartrate);
+        builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        builder.setContentIntent(resultPendingIntent);
+        NotificationManagerCompat notificationManagerCompat=NotificationManagerCompat.from(this);
+        notificationManagerCompat.notify(NOTIFICATION_ID,builder.build());
+    }
+    public void displayNotificationpulse(String pulse)
+    {
+        Intent resultIntent = new Intent(this, MainActivity.class);
+// Create the TaskStackBuilder and add the intent, which inflates the back stack
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addNextIntentWithParentStack(resultIntent);
+// Get the PendingIntent containing the entire back stack
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder builder=new NotificationCompat.Builder(this,CHANNEL_ID);
+        builder.setSmallIcon(R.drawable.notification_patient);
+        builder.setContentTitle("Pulse High Alert");
+        builder.setContentText(pulse);
+        builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        builder.setContentIntent(resultPendingIntent);
+        NotificationManagerCompat notificationManagerCompat=NotificationManagerCompat.from(this);
+        notificationManagerCompat.notify(NOTIFICATION_ID,builder.build());
+    }
+    public void displayNotificationtemperature(String temp)
+    {
+        Intent resultIntent = new Intent(this, MainActivity.class);
+// Create the TaskStackBuilder and add the intent, which inflates the back stack
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addNextIntentWithParentStack(resultIntent);
+// Get the PendingIntent containing the entire back stack
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder builder=new NotificationCompat.Builder(this,CHANNEL_ID);
+        builder.setSmallIcon(R.drawable.notification_patient);
+        builder.setContentTitle("Body High Temprature High");
+        builder.setContentText(temp);
+        builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        builder.setContentIntent(resultPendingIntent);
+        NotificationManagerCompat notificationManagerCompat=NotificationManagerCompat.from(this);
+        notificationManagerCompat.notify(NOTIFICATION_ID,builder.build());
+    }
 
     @Override
     protected void onStart() {
@@ -79,13 +145,13 @@ DatabaseReference mHumidityref=mRootref.child("Humidity");
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 Float text=dataSnapshot.getValue(Float.class);
-                lottieAnimationView.setVisibility(View.GONE);
+                lottieAnimationViewHumidity.setVisibility(View.GONE);
                 humidity.setVisibility(View.VISIBLE);
                 humidity.setText(String.valueOf(text));
 
                 if(text>300)
                 {
-                    displayNotification(text);
+                    displayNotificationhumidity(text);
                 }
 
             }
@@ -93,7 +159,76 @@ DatabaseReference mHumidityref=mRootref.child("Humidity");
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
+                lottieAnimationViewHumidity.setVisibility(View.GONE);
+                humidity.setVisibility(View.VISIBLE);
+                humidity.setText("No Data");
+            }
+        });
+        mHeartrateref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String text=dataSnapshot.getValue(String.class);
+                lottieAnimationViewheart.setVisibility(View.GONE);
+                heart.setVisibility(View.VISIBLE);
+                heart.setText(String.valueOf(text));
+//                if(Integer.parseInt(text)>300)
+//                {
+//                    displayNotificationheart(text);
+//                }
+            }
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                lottieAnimationViewHumidity.setVisibility(View.GONE);
+                humidity.setVisibility(View.VISIBLE);
+                humidity.setText("No Data");
+            }
+        });
+        mPulse.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                String text=dataSnapshot.getValue(String.class);
+                lottieAnimationViewpulse.setVisibility(View.GONE);
+                pulse.setVisibility(View.VISIBLE);
+                pulse.setText(String.valueOf(text));
+//                if(Integer.parseInt(text)>300)
+//                {
+//                    displayNotificationpulse(text);
+//                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                lottieAnimationViewHumidity.setVisibility(View.GONE);
+                humidity.setVisibility(View.VISIBLE);
+                humidity.setText("No Data");
+            }
+        });
+        mTemperature.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String text=dataSnapshot.getValue(String.class);
+                lottieAnimationViewtemp.setVisibility(View.GONE);
+                temp.setVisibility(View.VISIBLE);
+                temp.setText(String.valueOf(text));
+//                if(Integer.parseInt(text)>300)
+//                {
+//                    displayNotificationtemperature(text);
+//                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                lottieAnimationViewHumidity.setVisibility(View.GONE);
+                humidity.setVisibility(View.VISIBLE);
+                humidity.setText("No Data");
             }
         });
     }
+
 }
